214a84f7342084b54bafa834555f05f74efe5193
tranvanhuy-dev-it <tranvanhuy064206@gmail.com>
2026-08-25 14:11:07 +0700

    fix: import useRef in AuthPageWrapper

--- src/components/auth/AuthPageWrapper.tsx
+++ src/components/auth/AuthPageWrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
